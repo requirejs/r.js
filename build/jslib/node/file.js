4645cedf8a32c2d57117cf6da3f5b7ac4eee5bc2
jrburke <jrburke@gmail.com>
2011-10-02 21:04:26 -0700

    More windows accomodations, backslashes in copyDir function.

--- build/jslib/node/file.js
+++ build/jslib/node/file.js
@@ -142,9 +142,10 @@ define(['fs', 'path'], function (fs, path) {
             //file should be copied. Returns a list file name strings of the destinations that were copied.
             regExpFilter = regExpFilter || /\w/;
 
-            //Normalize th directory names.
-            srcDir = path.normalize(srcDir);
-            destDir = path.normalize(destDir);
+            //Normalize th directory names, but keep front slashes.
+            //path module on windows now returns backslashed paths.
+            srcDir = frontSlash(path.normalize(srcDir));
+            destDir = frontSlash(path.normalize(destDir));
 
             var fileNames = file.getFilteredFileList(srcDir, regExpFilter, true),
             copiedFiles = [], i, srcFileName, destFileName;
