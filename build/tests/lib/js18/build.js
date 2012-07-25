6ff6c7b375aee89a46776fd6ac3fd72c9ba37acf
jrburke <jrburke@gmail.com>
2012-07-25 16:07:42 -0700

    Allowing for more js 1.7/1.8 features, yield.

--- build/tests/lib/js18/build.js
+++ build/tests/lib/js18/build.js
@@ -7,12 +7,20 @@
     onBuildRead: function (id, path, contents) {
         //Remove destructuring so parsing works. Do this until esprima has
         //a fix for: http://code.google.com/p/esprima/issues/detail?id=241
-        var destructRegExp = /(var|let|const)\s*\{[^\}]+\}\s*=/g;
-        return contents.replace(destructRegExp, '/*DESTRUCT$&DESTRUCT*/');
+        var destructRegExp = /(var|let|const)\s*\{[^\}]+\}\s*=/g,
+            yieldRegExp = /\byield\b/g;
+
+        return contents
+            .replace(destructRegExp, '/*DESTRUCT$&DESTRUCT*/')
+            .replace(yieldRegExp, '/*YIELD*/');
     },
     onBuildWrite: function (id, path, contents) {
         //Restore destructuring
-        var restructRegExp = /\/\*DESTRUCT(.*)DESTRUCT\*\//g;
-        return contents.replace(restructRegExp, '$1');
+        var destructRegExp = /\/\*DESTRUCT(.*)DESTRUCT\*\//g,
+            yieldRegExp = /\/\*YIELD\*\//g;
+
+        return contents
+            .replace(destructRegExp, '$1')
+            .replace(yieldRegExp, 'yield');
     }
 })
