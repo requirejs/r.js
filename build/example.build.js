a7ced7958e01e445361833651e094ad82ea2f9e9
jrburke <jrburke@gmail.com>
2012-08-06 23:21:00 -0700

    Related to #242, document no_mangle in the example.build.js

--- build/example.build.js
+++ build/example.build.js
@@ -89,7 +89,12 @@
         toplevel: true,
         ascii_only: true,
         beautify: true,
-        max_line_length: 1000
+        max_line_length: 1000,
+
+        //Custom value supported by r.js but done differently
+        //in uglifyjs directly:
+        //Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
+        no_mangle: true
     },
 
     //If using Closure Compiler for script optimization, these config options
