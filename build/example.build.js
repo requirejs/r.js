fd4e69ef7d7be7c07994d602935708870b6838a2
jrburke <jrburke@gmail.com>
2012-09-06 16:03:18 -0700

    Fixes #273, document how to use uglifyjs defines

--- build/example.build.js
+++ build/example.build.js
@@ -91,6 +91,12 @@
         beautify: true,
         max_line_length: 1000,
 
+        //How to pass uglifyjs defined symbols for AST symbol replacement,
+        //see "defines" options for ast_mangle in the uglifys docs.
+        defines: {
+            DEBUG: ['name', 'false']
+        },
+
         //Custom value supported by r.js but done differently
         //in uglifyjs directly:
         //Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
