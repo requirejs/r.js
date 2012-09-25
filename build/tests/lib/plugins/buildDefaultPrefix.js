f980230b68504e801b8e47cefa0534ce1b943f87
jrburke <jrburke@gmail.com>
2012-09-25 15:26:07 -0700

    Relates to jrburke/requirejs#333, remove defaultPrefix support

--- build/tests/lib/plugins/buildDefaultPrefix.js
+++
@@ -1,7 +0,0 @@
-{
-    mainConfigFile: '../../../../../requirejs/tests/plugins/defaultPrefix/defaultPrefix-tests.js',
-    name: 'defaultPrefix-tests',
-    stubModules: ['text', 'refine'],
-    out: '../../../../../requirejs/tests/plugins/defaultPrefix/built/defaultPrefix-tests.js',
-    optimize: 'none'
-}
