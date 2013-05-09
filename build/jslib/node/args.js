32d1306bf53a62401ac2b78d7153b09a874064be
jrburke <jrburke@gmail.com>
2013-05-08 21:01:11 -0700

    Fixes jrburke/requirejs#700, silly propagated typo

--- build/jslib/node/args.js
+++ build/jslib/node/args.js
@@ -11,8 +11,8 @@ define(function () {
     //Do not return the "node" or "r.js" arguments
     var args = process.argv.slice(2);
 
-    //Ignore any command option used for rq.js
-    if (args[0] && args[0].indexOf('-' === 0)) {
+    //Ignore any command option used for main x.js branching
+    if (args[0] && args[0].indexOf('-') === 0) {
         args = args.slice(1);
     }
 
