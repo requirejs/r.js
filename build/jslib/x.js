5092541c4e8ec33159b0d7ad4c36ffe50e6e79df
jrburke <jrburke@gmail.com>
2013-07-07 22:03:37 -0700

    Rev for 2.1.7

--- build/jslib/x.js
+++ build/jslib/x.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.6+ Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.7 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -20,7 +20,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.1.6+',
+        version = '2.1.7',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
