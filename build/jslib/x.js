32247b1be7238e553a40e8186629d5d9287d812a
jrburke <jrburke@gmail.com>
2013-10-23 15:30:34 -0700

    update copyright years

--- build/jslib/x.js
+++ build/jslib/x.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.9 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.9 Copyright (c) 2010-2013, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
