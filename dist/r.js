87ba6daa4bb72aabb13621424aa22e3a5bb20a88
jrburke <jrburke@gmail.com>
2013-12-31 00:09:19 -0800

    snapshot

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.9+ Tue, 31 Dec 2013 03:35:30 GMT Copyright (c) 2010-2013, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.9+ Tue, 31 Dec 2013 08:07:36 GMT Copyright (c) 2010-2013, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -20,7 +20,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.1.9+ Tue, 31 Dec 2013 03:35:30 GMT',
+        version = '2.1.9+ Tue, 31 Dec 2013 08:07:36 GMT',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -24878,7 +24878,7 @@ define('build', function (require) {
     build.makeRelativeFilePath = function (refPath, targetPath) {
         var i, dotLength, finalParts, length,
             refParts = refPath.split('/'),
-            targetParts = targetPath.split('/'),
+            targetParts = file.normalize(targetPath).split('/'),
             //Pull off file name
             targetName = targetParts.pop(),
             dotParts = [];
