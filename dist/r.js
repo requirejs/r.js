363ac76f894e468a146c9f581a1b0003177148d2
jrburke <jrburke@gmail.com>
2014-01-05 22:55:54 -0800

    snapshot

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.9+ Mon, 06 Jan 2014 06:06:17 GMT Copyright (c) 2010-2013, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.9+ Mon, 06 Jan 2014 06:55:42 GMT Copyright (c) 2010-2013, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -20,7 +20,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.1.9+ Mon, 06 Jan 2014 06:06:17 GMT',
+        version = '2.1.9+ Mon, 06 Jan 2014 06:55:42 GMT',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -2629,16 +2629,26 @@ var requirejs, require, define, xpcUtil;
  */
 
 /*jslint plusplus: true */
-/*global define */
+/*global define, java */
 
 define('lang', function () {
     'use strict';
 
-    var lang,
+    var lang, isJavaObj,
         hasOwn = Object.prototype.hasOwnProperty;
 
     function hasProp(obj, prop) {
         return hasOwn.call(obj, prop);
+    }
+
+    isJavaObj = function () {
+        return false;
+    };
+
+    if (typeof java !== 'undefined' && java.lang && java.lang.Object) {
+        isJavaObj = function (obj) {
+            return obj instanceof java.lang.Object;
+        };
     }
 
     lang = {
@@ -2726,7 +2736,7 @@ define('lang', function () {
             type = typeof obj;
             if (obj === null || obj === undefined || type === 'boolean' ||
                 type === 'string' || type === 'number' || lang.isFunction(obj) ||
-                lang.isRegExp(obj)) {
+                lang.isRegExp(obj)|| isJavaObj(obj)) {
                 return obj;
             }
 
