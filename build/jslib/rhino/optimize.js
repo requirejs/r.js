36e4c37ac0d964e8cfd4bd80326014e7796ec0fd
Konrad Lipner <k.lipner@samsung.com>
2014-03-20 09:57:20 +0100

    JS source file fetching Java class order reverted

--- build/jslib/rhino/optimize.js
+++ build/jslib/rhino/optimize.js
@@ -44,12 +44,12 @@ define(['logger', 'env!env/file'], function (logger, file) {
 
     //Bind to Closure compiler, but if it is not available, do not sweat it.
     try {
-        // Try for newer closure compiler that needs Java 7+
-        JSSourceFilefromCode = java.lang.Class.forName('com.google.javascript.jscomp.SourceFile').getMethod('fromCode', [java.lang.String, java.lang.String]);
-    } catch (e) {
         // Try older closure compiler that worked on Java 6
+        JSSourceFilefromCode = java.lang.Class.forName('com.google.javascript.jscomp.JSSourceFile').getMethod('fromCode', [java.lang.String, java.lang.String]);
+    } catch (e) {
         try {
-            JSSourceFilefromCode = java.lang.Class.forName('com.google.javascript.jscomp.JSSourceFile').getMethod('fromCode', [java.lang.String, java.lang.String]);
+            // Try for newer closure compiler that needs Java 7+
+            JSSourceFilefromCode = java.lang.Class.forName('com.google.javascript.jscomp.SourceFile').getMethod('fromCode', [java.lang.String, java.lang.String]);
         } catch (e) {}
     }
 
