83d7ffafb7ab05279c6b780816674ca68e4e03aa
jrburke <jrburke@gmail.com>
2014-05-26 15:20:08 -0700

    snapshot

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.11+ Mon, 26 May 2014 21:28:31 GMT Copyright (c) 2010-2014, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.11+ Mon, 26 May 2014 22:20:01 GMT Copyright (c) 2010-2014, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -20,7 +20,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.1.11+ Mon, 26 May 2014 21:28:31 GMT',
+        version = '2.1.11+ Mon, 26 May 2014 22:20:01 GMT',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -2455,7 +2455,7 @@ var requirejs, require, define, xpcUtil;
 
                     //Break any cycles by requiring it normally, but this will
                     //finish synchronously
-                    require([moduleName]);
+                    context.require([moduleName]);
 
                     //The above calls are sync, so can do the next thing safely.
                     ret = context.defined[moduleName];
