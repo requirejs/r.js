4283c21ef81f72d793eba7515a3b1d0dcac68974
Skyline0705 <Skyline0705@Skyline0705deMacBook-Pro.local>
2014-10-28 11:28:54 +0800

    fix the build bug about AMD Dependence，change the endLine from '\n' to '/\r\n|\r|\n/'

--- build/jslib/transform.js
+++ build/jslib/transform.js
@@ -216,7 +216,7 @@ function (esprima, parse, logger, lang) {
             //further up.
             defineInfos.reverse();
 
-            contentLines = contents.split('\n');
+            contentLines = contents.split(/\r\n|\r|\n/);
 
             modLine = function (loc, contentInsertion) {
                 var startIndex = loc.start.column,
