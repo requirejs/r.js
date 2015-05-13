7ac040aec81016cf849e6b18a97f920397d831e4
jrburke <jrburke@gmail.com>
2015-05-13 10:34:05 -0700

    snapshot

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.1.17+ Wed, 06 May 2015 00:31:15 GMT Copyright (c) 2010-2015, The Dojo Foundation All Rights Reserved.
+ * @license r.js 2.1.17+ Wed, 13 May 2015 17:33:53 GMT Copyright (c) 2010-2015, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/requirejs for details
  */
@@ -20,7 +20,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.1.17+ Wed, 06 May 2015 00:31:15 GMT',
+        version = '2.1.17+ Wed, 13 May 2015 17:33:53 GMT',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -2929,7 +2929,8 @@ var prim;
     function check(p) {
         if (hasProp(p, 'e') || hasProp(p, 'v')) {
             if (!prim.hideResolutionConflict) {
-                throw new Error('nope');
+                throw new Error('Prim promise already resolved: ' +
+                                JSON.stringify(p));
             }
             return false;
         }
