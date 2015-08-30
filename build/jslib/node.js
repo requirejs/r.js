2d4c7172930c38266fe89f72ab01e6abd9e4f198
Avi Kohn <h.e.kohn824@gmail.com>
2015-08-30 14:14:44 -0400

    Move `vm.runInThisContext` to require.exec
    In split contexts, like NW.js, runInThisContext evaluates modules in the wrong context.  And, due to its location, this behavior is impossible to override.
    
    This commit moves the call to require.exec, which is overridable, and changes makeNodeWrapper to use `global.requirejsVars` instead of just requirejsVars.

--- build/jslib/node.js
+++ build/jslib/node.js
@@ -105,7 +105,7 @@
     req.makeNodeWrapper = function (contents) {
         return '(function (require, requirejs, define) { ' +
                 contents +
-                '\n}(requirejsVars.require, requirejsVars.requirejs, requirejsVars.define));';
+                '\n}(global.requirejsVars.require, global.requirejsVars.requirejs, global.requirejsVars.define));';
     };
 
     req.load = function (context, moduleName, url) {
@@ -120,9 +120,8 @@
         if (exists(url)) {
             contents = fs.readFileSync(url, 'utf8');
 
-            contents = req.makeNodeWrapper(contents);
             try {
-                vm.runInThisContext(contents, fs.realpathSync(url));
+                req.exec(contents, fs.realpathSync(url));
             } catch (e) {
                 err = new Error('Evaluating ' + url + ' as module "' +
                                 moduleName + '" failed with error: ' + e);
@@ -170,9 +169,8 @@
     };
 
     //Override to provide the function wrapper for define/require.
-    req.exec = function (text) {
-        /*jslint evil: true */
+    req.exec = function (text, realPath) {
         text = req.makeNodeWrapper(text);
-        return eval(text);
+        return vm.runInThisContext(text, realPath);
     };
 }());
