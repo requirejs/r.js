d8f30c1ed9404c20dca6794222312d0d77b16831
Kevan Davis <kevan.davis@handshake.com>
2016-07-07 14:31:36 -0400

    Release 2.2.1

--- dist/r.js
+++ dist/r.js
@@ -1,5 +1,5 @@
 /**
- * @license r.js 2.2.0 Copyright jQuery Foundation and other contributors.
+ * @license r.js 2.2.1 Copyright jQuery Foundation and other contributors.
  * Released under MIT license, http://github.com/requirejs/r.js/LICENSE
  */
 
@@ -19,7 +19,7 @@ var requirejs, require, define, xpcUtil;
 (function (console, args, readFileFunc) {
     var fileName, env, fs, vm, path, exec, rhinoContext, dir, nodeRequire,
         nodeDefine, exists, reqMain, loadedOptimizedLib, existsForNode, Cc, Ci,
-        version = '2.2.0',
+        version = '2.2.1',
         jsSuffixRegExp = /\.js$/,
         commandOption = '',
         useLibLoaded = {},
@@ -3569,7 +3569,7 @@ define('node/file', ['fs', 'path', 'prim'], function (fs, path, prim) {
                     } else if (stat.isDirectory() &&
                               (!file.exclusionRegExp || !file.exclusionRegExp.test(fileName))) {
                         dirFiles = this.getFilteredFileList(filePath, regExpFilters, makeUnixPaths);
-                        files.push.apply(files, dirFiles);
+                        files = files.concat(dirFiles);
                     }
                 }
             }
