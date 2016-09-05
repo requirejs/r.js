b79a3393aa1f198bfea373d5c14feb60a33c247e
Josip Janzic <josip.janzic@gmail.com>
2016-09-05 11:57:50 +0200

    Fix the syntax error on decleration of `addFile()`
    Fixes #928

--- build/jslib/uglifyjs.js
+++ build/jslib/uglifyjs.js
@@ -9075,7 +9075,7 @@ exports.minify = function(files, options, name) {
     if (options.spidermonkey) {
         toplevel = AST_Node.from_mozilla_ast(files);
     } else {
-        function addFile(file, fileUrl) {
+        var addFile = function(file, fileUrl) {
             var code = options.fromString
                 ? file
                 : rjsFile.readFile(file, "utf8");
